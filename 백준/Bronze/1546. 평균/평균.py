ace86f8e2bdc2b5ef380d4410a4d0e309251ac40
MuchanKim <91643099+MuchanKim@users.noreply.github.com>
2023-09-17 17:01:37 +0900

    Update 평균.py

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,19 +1,27 @@
-def resultScore(score, highest_score, num_of_scores):
+# 점수 리스트 생성 함수
+def create_scores_list(num_of_scores):
+    while True:
+        # 공백을 기준으로 분리하여 리스트에 저장
+        scores= [int(score) for score in input().split()]
+        
+        # 입력받은 숫자의 개수와 조건에 맞는지 확인
+        if num_of_scores == len(scores) and max(scores) >=0:
+            return scores
+        else:
+            print(f"{num_of_scores}개의 숫자를 입력해주세요. 음이 아닌 정수이고, 적어도 한개는 0보다 커야합니다.")
+
+# 평균 점수 계산 함수
+def calculate_average_score(score):
+    highest_score = max(score) # 최고 점수
     result = 0
+    
     for num in score:
         result += (num / highest_score) * 100
     return result / num_of_scores
 
 num_of_scores = int(input())
 
-while True:
-    scores= [int(score) for score in input().split()]
-    max_num = max(scores)
+scores = create_scores_list(num_of_scores)
+avr_grade = calculate_average_score(scores)
 
-    if num_of_scores == len(scores) and max_num >=0:
-        break
-    else:
-        print(f"{num_of_scores}개의 숫자를 입력해주세요. 음이 아닌 정수이고, 적어도 한개는 0보다 커야합니다.")
-        
-avr_grade = resultScore(scores, max_num, num_of_scores)
 print(avr_grade)
