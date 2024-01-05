648117599ac4ce302b76c0082e65cea66a2f96d5
MuchanKim <91643099+MuchanKim@users.noreply.github.com>
2024-01-05 11:37:04 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -2,20 +2,12 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-money = 1000 - n
-count = 0
+change_money = 1000 - int(input())
+coins = (500, 100, 50, 10, 5, 1)
+count =0
 
-count += int(money / 500)
-money %= 500
-count += int(money / 100)
-money %= 100
-count += int(money / 50)
-money %= 50
-count += int(money / 10)
-money %= 10
-count += int(money / 5)
-money %= 5
-count += int(money / 1)
+for coin in coins:
+    count += change_money // coin
+    change_money %= coin
 
 print(count)
